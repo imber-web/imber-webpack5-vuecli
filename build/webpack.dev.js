621fecdf52cfd0df8f1fdf0e142047de051b2d84
imber <a1157911285@163.com>
2022-06-23 10:39:55 +0800

    feat：添加环境变量功能

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -2,7 +2,7 @@
 
 const { merge } = require('webpack-merge')
 const base = require('./webpack.base')
-
+const webpack = require('webpack')
 module.exports = merge(base, {
   mode: 'development',
   devServer: {
@@ -10,5 +10,17 @@ module.exports = merge(base, {
     // hot: true,
   },
   // 使用eval-cheap-module-source-map模式，能具体定位到源码位置和源码展示，适合开发模式，体积较小
-  devtool: 'eval-cheap-module-source-map'
+  devtool: 'eval-cheap-module-source-map',
+  plugins: [
+    // 定义全局变量
+    new webpack.DefinePlugin({
+      process: {
+        env: {
+          NODE_DEV: JSON.stringify('development')
+          // 这里可以定义你的环境变量
+          // VUE_APP_URL: JSON.stringify('https://xxx.com')
+        }
+      }
+    })
+  ]
 })
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -6,5 +6,17 @@ const base = require('./webpack.base')
 module.exports = merge(base, {
   mode: 'production',
   // 使用nosources-source-map，只能定位源码位置，不能源码展示，体积较小，适合生产模式
-  devtool: 'nosources-source-map'
+  devtool: 'nosources-source-map',
+  plugins: [
+    // 定义全局变量
+    new webpack.DefinePlugin({
+      process: {
+        env: {
+          NODE_DEV: JSON.stringify('prodction')
+          // 这里可以定义你的环境变量
+          // VUE_APP_URL: JSON.stringify('https://xxx.com')
+        }
+      }
+    })
+  ]
 })
